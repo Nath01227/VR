44b94ed36a4f359c87b973ff70953353123cab96
gehlisboa <gehmoraeslisboa@gmail.com>
2025-09-26 15:27:14 -0300

    Modelos Estaticos TestL

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AR2
+Creditos para: https://github.com/nicolocarpignoli/location-based-ar-tutorial
--- script.js
+++ script.js
@@ -1,68 +1,21 @@
-// getting places from APIs
-function loadPlaces(position) {
-    const params = {
-        radius: 15,    // search places not farther than this value (in meters)
-        clientId: 'MVBNK5KJB3SVVNUREH4LBR5FYMHE3KMG0KOOKM0IFAKYARN2',
-        clientSecret: 'HSPBMCDA4IZWCCIJHWE5RFYOLRLGAVTNRO3KTGR2QOGPPFOZ',
-        version: '20300101',    // foursquare versioning, required but unuseful for this demo
-    };
-
-    // CORS Proxy to avoid CORS problems
-    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-    // Foursquare API (limit param: number of maximum places to fetch)
-    const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-        &ll=${position.latitude},${position.longitude}
-        &radius=${params.radius}
-        &client_id=${params.clientId}
-        &client_secret=${params.clientSecret}
-        &limit=30 
-        &v=${params.version}`;
-    return fetch(endpoint)
-        .then((res) => {
-            return res.json()
-                .then((resp) => {
-                    return resp.response.venues;
-                })
-        })
-        .catch((err) => {
-            console.error('Error with places API', err);
-        })
-};
-
-
-window.onload = () => {
-    const scene = document.querySelector('a-scene');
-
-    // first get current user location
-    return navigator.geolocation.getCurrentPosition(function (position) {
-
-        // than use it to load from remote APIs some places nearby
-        loadPlaces(position.coords)
-            .then((places) => {
-                places.forEach((place) => {
-                    const latitude = place.location.lat;
-                    const longitude = place.location.lng;
-
-                    // add place name
-                    const placeText = document.createElement('a-link');
-                    placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-                    placeText.setAttribute('title', place.name);
-                    placeText.setAttribute('scale', '15 15 15');
-                    
-                    placeText.addEventListener('loaded', () => {
-                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-                    });
-
-                    scene.appendChild(placeText);
-                });
-            })
+AFRAME.registerComponent("static-anchor", {
+    init: function () {
+      const el = this.el;
+      const eventName = "gps-entity-place-update-positon";
+      el.addEventListener(
+        eventName,
+        (event) => {
+          const distance = event.detail && event.detail.distance;
+          console.log("Initial placement distance:", distance);
+          const gpsComp =
+            el.components["gps-new-entity-place"] ||
+            el.components["gps-entity-place"];
+          if (gpsComp && gpsComp._updatePosition) {
+            gpsComp._updatePosition = function () {
+            };
+          }
+        },
+        { once: true }
+      );
     },
-        (err) => console.error('Error in retrieving position', err),
-        {
-            enableHighAccuracy: true,
-            maximumAge: 0,
-            timeout: 27000,
-        }
-    );
-};
+  });
